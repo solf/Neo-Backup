163e6e658858783a59f07423d7f431aee63554f5
solf <solf@users.noreply.github.com>
2025-11-22 10:20:52 -0400

    Version bump to 9.0.0 due to storage format incompatibility (apk dedup)

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,10 +48,10 @@ android {
         applicationId = "com.machiav3lli.backup"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8327
-        versionName = "8.3.14"
-        buildConfigField("int", "MAJOR", "8")
-        buildConfigField("int", "MINOR", "3")
+        versionCode = 9000
+        versionName = "9.0.0"
+        buildConfigField("int", "MAJOR", "9")
+        buildConfigField("int", "MINOR", "0")
         buildConfigField("String[]", "DETECTED_LOCALES", langsListString)
 
         testApplicationId = "$applicationId.tests"
